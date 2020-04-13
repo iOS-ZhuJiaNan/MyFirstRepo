e95019008970fa917b20d6381d279285ec951e13
朱佳男 <754148169@qq.com>
2020-04-13 12:09:57 +0800

    提交文件

---
+++ Class/MyFirstObject.h
@@ -0,0 +1,17 @@
+//
+//  MyFirstObject.h
+//  YXDNAUIDemo
+//
+//  Created by 谦卦 on 2020/4/13.
+//  Copyright © 2020 ZJN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MyFirstObject : NSObject
+-(void)logMyStr:(NSString *)str;
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Class/MyFirstObject.m
@@ -0,0 +1,15 @@
+//
+//  MyFirstObject.m
+//  YXDNAUIDemo
+//
+//  Created by 谦卦 on 2020/4/13.
+//  Copyright © 2020 ZJN. All rights reserved.
+//
+
+#import "MyFirstObject.h"
+
+@implementation MyFirstObject
+-(void)logMyStr:(NSString *)str{
+    NSLog(@"这是我的第一个repo");
+}
+@end
